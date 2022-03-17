7d0b2276719c462727f6ac14ee1b0cbe08ac7a9f
Kevin Yang <91995544+kevinYang0612@users.noreply.github.com>
2022-03-17 00:07:36 -0700

    Update SortAlgorithms.java

--- SortAlgorithms.java
+++ SortAlgorithms.java
@@ -144,7 +144,220 @@ public class SortAlgorithms
         array[i] = array[j];
         array[j] = temp;
     }
-        public static void sortBooleanArray(boolean [] arr) {
+        
+    public static int[] merge3Sorted(int[] A, int[] B, int[] C)
+    {
+        int[] result = new int[A.length + B.length + C.length];
+        int p = 0;
+        int i = 0, j = 0, k = 0;
+        while (i < A.length && j < B.length && k < C.length)
+        {
+            int a = A[i];
+            int b = B[j];
+            int c = C[k];
+            if (a <= b && a <= c)
+            {
+                result[p] = a;
+                i++;
+            }
+            else if (b <= a && b <= c)
+            {
+                result[p] = b;
+                j++;
+            }
+            else
+            {
+                result[p] = c;
+                k++;
+            }
+            p++;
+        }
+        while (i < A.length && j < B.length)
+        {
+            if (A[i] < B[j])
+            {
+                result[p] = A[i];
+                i++;
+            }
+            else
+            {
+                result[p] = B[j];
+                j++;
+            }
+            p++;
+        }
+        while (j < B.length && k < C.length)
+        {
+            if (B[j] < C[k])
+            {
+                result[p] = B[j];
+                j++;
+            }
+            else
+            {
+                result[p] = C[k];
+                k++;
+            }
+            p++;
+        }
+        while (i < A.length && k < C.length)
+        {
+            if (A[i] < C[k])
+            {
+                result[p] = A[i];
+                i++;
+            }
+            else
+            {
+                result[p] = C[k];
+                k++;
+            }
+            p++;
+        }
+        while (i < A.length)
+        {
+            result[p] = A[i];
+            i++;
+            p++;
+        }
+        while (j < B.length) {
+            result[p] = B[j];
+            j++;
+            p++;
+        }
+        while (k < C.length)
+        {
+            result[p] = C[k];
+            k++;
+            p++;
+        }
+        return result;
+    }
+    public static void mergeSort3Way(Integer[] gArray)
+    {
+        // if array of size is zero returns null
+        if (gArray == null)
+            return;
+
+        // creating duplicate of given array
+        Integer[] fArray = new Integer[gArray.length];
+
+        // copying elements of given array into
+        // duplicate array
+        for (int i = 0; i < fArray.length; i++)
+            fArray[i] = gArray[i];
+
+        // sort function
+        mergeSort3WayRec(fArray, 0, gArray.length, gArray);
+
+        // copy back elements of duplicate array
+        // to given array
+        for (int i = 0; i < fArray.length; i++)
+            gArray[i] = fArray[i];
+    }
+
+    /* Performing the merge sort algorithm on the
+       given array of values in the rangeof indices
+       [low, high).  low is minimum index, high is
+       maximum index (exclusive) */
+    public static void mergeSort3WayRec(Integer[] gArray,
+                                        int low, int high, Integer[] destArray)
+    {
+        // If array size is 1 then do nothing
+        if (high - low < 2)
+            return;
+
+        // Splitting array into 3 parts
+        int mid1 = low + ((high - low) / 3);
+        int mid2 = low + 2 * ((high - low) / 3) + 1;
+
+        // Sorting 3 arrays recursively
+        mergeSort3WayRec(destArray, low, mid1, gArray);
+        mergeSort3WayRec(destArray, mid1, mid2, gArray);
+        mergeSort3WayRec(destArray, mid2, high, gArray);
+
+        // Merging the sorted arrays
+        merge(destArray, low, mid1, mid2, high, gArray);
+    }
+
+    /* Merge the sorted ranges [low, mid1), [mid1,
+       mid2) and [mid2, high) mid1 is first midpoint
+       index in overall range to merge mid2 is second
+       midpoint index in overall range to merge*/
+    public static void merge(Integer[] gArray, int low,
+                             int mid1, int mid2, int high,
+                             Integer[] destArray)
+    {
+        int i = low, j = mid1, k = mid2, l = low;
+
+        // choose smaller of the smallest in the three ranges
+        while ((i < mid1) && (j < mid2) && (k < high))
+        {
+            if (gArray[i].compareTo(gArray[j]) < 0)
+            {
+                if (gArray[i].compareTo(gArray[k]) < 0)
+                    destArray[l++] = gArray[i++];
+
+                else
+                    destArray[l++] = gArray[k++];
+            }
+            else
+            {
+                if (gArray[j].compareTo(gArray[k]) < 0)
+                    destArray[l++] = gArray[j++];
+                else
+                    destArray[l++] = gArray[k++];
+            }
+        }
+
+        // case where first and second ranges have
+        // remaining values
+        while ((i < mid1) && (j < mid2))
+        {
+            if (gArray[i].compareTo(gArray[j]) < 0)
+                destArray[l++] = gArray[i++];
+            else
+                destArray[l++] = gArray[j++];
+        }
+
+        // case where second and third ranges have
+        // remaining values
+        while ((j < mid2) && (k < high))
+        {
+            if (gArray[j].compareTo(gArray[k]) < 0)
+                destArray[l++] = gArray[j++];
+
+            else
+                destArray[l++] = gArray[k++];
+        }
+
+        // case where first and third ranges have
+        // remaining values
+        while ((i < mid1) && (k < high))
+        {
+            if (gArray[i].compareTo(gArray[k]) < 0)
+                destArray[l++] = gArray[i++];
+            else
+                destArray[l++] = gArray[k++];
+        }
+
+        // copy remaining values from the first range
+        while (i < mid1)
+            destArray[l++] = gArray[i++];
+
+        // copy remaining values from the second range
+        while (j < mid2)
+            destArray[l++] = gArray[j++];
+
+        // copy remaining values from the third range
+        while (k < high)
+            destArray[l++] = gArray[k++];
+    }
+
+    
+    
+    public static void sortBooleanArray(boolean [] arr) 
+    {
         int [] m = new int [2];
         for(Boolean b : arr)
         {
